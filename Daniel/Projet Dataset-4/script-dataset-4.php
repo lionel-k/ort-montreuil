b4c4034901eccfc80e069edd599d50d50f0ee32c
dani <soudry.daniel99@gmail.com>
2021-11-21 15:02:27 +0100

    final commit

--- Daniel/Projet Dataset-4/script-dataset-4.php
+++ Daniel/Projet Dataset-4/script-dataset-4.php
@@ -1,7 +1,9 @@
 <?php
 include './vendor/autoload.php';
 
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Reader\Csv;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 //BDD
 $servername = "localhost";
@@ -33,14 +35,14 @@ foreach ($sheetData as $key => $contrat) {
         $tbl_ingredient = explode(",", $lst_ingredient);
 
 
-        $req = "INSERT INTO `contrat`(`contract_id`, `hotel_name`) VALUES ('" . $contract_id . "', '" . $hotel_name . "')";
+        $req = "INSERT IGNORE INTO `contrat`(`contract_id`, `hotel_name`) VALUES ('" . $contract_id . "', '" . $hotel_name . "')";
 
         if ($conn->query($req) !== TRUE) {
             //echo "Error: " . $req . "<br>" . $conn->error;
         }
 
         foreach ($tbl_ingredient as $key => $ingredient) {
-            $req2 = "INSERT INTO `contrat_ingredient`(`contract_id`, `ingredient_name`) VALUES ('" . $contract_id . "', '" . $ingredient . "')";
+            $req2 = "INSERT IGNORE INTO `contrat_ingredient`(`contract_id`, `ingredient_name`) VALUES ('" . $contract_id . "', '" . $ingredient . "')";
 
             if ($conn->query($req2) !== TRUE) {
                 //echo "Error: " . $req2 . "<br>" . $conn->error;
@@ -62,14 +64,14 @@ foreach ($jsonData as $key => $value) {
 
     $tbl_ingredient = explode(",", $lst_ingredient);
 
-    $req = "INSERT INTO `contrat`(`contract_id`, `hotel_name`) VALUES ('" . $contract_id . "', '" . $hotel_name . "')";
+    $req = "INSERT IGNORE INTO `contrat`(`contract_id`, `hotel_name`) VALUES ('" . $contract_id . "', '" . $hotel_name . "')";
 
     if ($conn->query($req) !== TRUE) {
         echo "Error: " . $req . "<br>" . $conn->error;
     }
 
     foreach ($tbl_ingredient as $key => $ingredient) {
-        $req2 = "INSERT INTO `contrat_ingredient`(`contract_id`, `ingredient_name`) VALUES ('" . $contract_id . "', '" . $ingredient . "')";
+        $req2 = "INSERT IGNORE INTO `contrat_ingredient`(`contract_id`, `ingredient_name`) VALUES ('" . $contract_id . "', '" . $ingredient . "')";
 
         if ($conn->query($req2) !== TRUE) {
             echo "Error: " . $req2 . "<br>" . $conn->error;
@@ -91,14 +93,14 @@ foreach ($dataXml as $key => $value) {
 
     $tbl_ingredient = explode(",", $lst_ingredient);
 
-    $req = "INSERT INTO `contrat`(`contract_id`, `hotel_name`) VALUES ('" . $contract_id . "', '" . $hotel_name . "')";
+    $req = "INSERT IGNORE INTO `contrat`(`contract_id`, `hotel_name`) VALUES ('" . $contract_id . "', '" . $hotel_name . "')";
 
     if ($conn->query($req) !== TRUE) {
         echo "Error: " . $req . "<br>" . $conn->error;
     }
 
     foreach ($tbl_ingredient as $key => $ingredient) {
-        $req2 = "INSERT INTO `contrat_ingredient`(`contract_id`, `ingredient_name`) VALUES ('" . $contract_id . "', '" . $ingredient . "')";
+        $req2 = "INSERT IGNORE INTO `contrat_ingredient`(`contract_id`, `ingredient_name`) VALUES ('" . $contract_id . "', '" . $ingredient . "')";
 
         if ($conn->query($req2) !== TRUE) {
             echo "Error: " . $req2 . "<br>" . $conn->error;
@@ -110,8 +112,44 @@ foreach ($dataXml as $key => $value) {
 //Requete SQL pour sélectionner les hôtels à qui on n'a pas encore vendu d'ingrédients
 $select = "SELECT distinct hotels.hotel_name FROM hotels WHERE hotels.hotel_name NOT IN( SELECT distinct hotels.hotel_name FROM contrat_ingredient INNER JOIN ingredients ON contrat_ingredient.`ingredient_name` = ingredients.`ingredient_name` INNER JOIN contrat ON contrat_ingredient.`contract_id` = contrat.`contract_id` INNER JOIN hotels ON contrat.hotel_name = hotels.hotel_name )";
 
-if ($conn->query($select) !== TRUE) {
-    echo "Error: " . $select . "<br>" . $conn->error;
-}
+if ($conn->query($select) !== false) {
+    $result = $conn->query($select);
 
-var_dump($select);
+    $spreadSheetWriter = new Spreadsheet();
+
+    $sheet = $spreadSheetWriter->getActiveSheet();
+    $spreadSheetWriter->getDefaultStyle()
+        ->getFont()
+        ->setName('Arial')
+        ->setSize(12);
+
+    // Coordonnées cellules
+    $lettre  = 'A';
+    $chiffre = '1';
+    $cellule = $lettre . $chiffre;
+
+
+    while ($row = $result->fetch_assoc()) {
+
+        $spreadSheetWriter->getActiveSheet()->getRowDimension($chiffre)->setRowHeight(20);
+        $spreadSheetWriter->getActiveSheet()
+            ->getColumnDimension("$lettre")->setWidth(20);
+
+        $spreadSheetWriter->getActiveSheet()
+            ->setCellValue("$cellule", $row["hotel_name"]);
+
+        //on passe au chiffre suivant et on l'applique à la position cellule
+        $chiffre = $chiffre + 1;
+        $cellule = $lettre . $chiffre;
+    }
+
+    //on prend la date pour la mettre dans le nom du fichier
+    date_default_timezone_set('Europe/Paris');
+    $date = date('Y-m-d');
+    $nomFichier = "Hôtels list - " . $date;
+
+    $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadSheetWriter);
+    $writer->save("docs/" . $nomFichier . ".csv");
+} else {
+    throw new Exception("Error Select", 1);
+}
