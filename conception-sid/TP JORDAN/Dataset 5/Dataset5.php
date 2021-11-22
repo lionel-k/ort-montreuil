f4096f1dc1f580291c1c48fd21e9d87af29965bd
jorcoh <59610747+jorcoh@users.noreply.github.com>
2021-11-22 16:24:03 +0100

    Update Dataset5.php

--- conception-sid/TP JORDAN/Dataset 5/Dataset5.php
+++ conception-sid/TP JORDAN/Dataset 5/Dataset5.php
@@ -1,5 +1,38 @@
 <?php
 
-// PARSE FICHIER JSON
+// PARSE FICHIER SQL
+
+try
+
+{
+       // On se connecte à MySQL
+       $bdd = new PDO('mysql:host=localhost;dbname=bddtp2', 'root', 'root');
+
+}
+
+catch(Exception $e)
+
+{
+
+       // En cas d'erreur, on affiche un message et on arrête tout
+       die('Erreur : '.$e->getMessage());
+
+}
+
+       
+
+// Si tout va bien, on peut continuer
+
+
+// On récupère tout le contenu de la table jeux_video
+
+$reponse = $bdd->query('SELECT * FROM jeux_video');
+
+
+// On affiche chaque entrée une à une
+
+while ($donnees = $reponse->fetch())
+
+{
 
 ?>
