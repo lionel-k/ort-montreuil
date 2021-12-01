492f5b5dc2799ecdca79a17b95fcf325093b4b22
Lionel Kubwimana <lionel.kubwimana@doctolib.com>
2021-12-02 00:29:52 +0100

    question 1 done

--- conception-sid/solution-5.rb
+++ conception-sid/solution-5.rb
@@ -7,8 +7,8 @@ rows = CSV.parse(File.read("dataset-5.csv"), headers: true)
 # 1. What are the transportation modes used per country ?
 def transportation_modes_per_country(rows)
   result = {}
-  rows.first(12).each do |row|
-    country = row['location'].downcase.split(Regexp.union(DELIMITERS)).last
+  rows.each do |row|
+    country = row['location'].downcase.split(Regexp.union(DELIMITERS)).last.gsub(/\s/,'-')
     transportation_modes = row['transportation_modes'].downcase.split(Regexp.union(DELIMITERS))
     if result.key?(country)
       transportation_modes_of_country = result[country]
@@ -23,8 +23,14 @@ def transportation_modes_per_country(rows)
       result[country] = transportation_modes.map { |mode | [mode, 1]}.to_h
     end
   end
-  result
-  binding.pry
+
+  # Export to CSV
+  result.each do |country, transportation_modes|
+    filename = "solution-5/question-1/transportation-modes-#{country}.csv"
+    values = transportation_modes.to_a.sort_by { |mode, count| mode }
+    values.unshift(['transportation_mode', 'count'])
+    File.write(filename, values.map(&:to_csv).join)
+  end
 end
 
 transportation_modes_per_country(rows)
