e6b2bc7f5d824e9ef87e095ee7cc02520e478288
Lionel Kubwimana <lionel.kubwimana@doctolib.com>
2021-12-02 01:07:46 +0100

    add solution to question-2

--- conception-sid/solution-5.rb
+++ conception-sid/solution-5.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'json'
 require 'pry'
 
 DELIMITERS = [' | ',' - ',' ; ']
@@ -33,4 +34,34 @@ def transportation_modes_per_country(rows)
   end
 end
 
-transportation_modes_per_country(rows)
+# transportation_modes_per_country(rows)
+
+# 2. How much people paid for hotel per country ?
+def countries_revenues(rows)
+  file = File.read('dataset-5-hotel-prices.json')
+  hotel_prices = JSON.parse(file).map do |hotel_price|
+    [hotel_price['hotel'], hotel_price['price_per_night']]
+  end.to_h
+  result = {}
+  rows.each do |row|
+    country_name = row['location'].split(Regexp.union(DELIMITERS)).last
+    country_slug = country_name.downcase.gsub(/\s/,'-')
+    arrival_date = Date.strptime(row['arrival_date'], "%m-%d-%Y")
+    departure_date = Date.parse(row['departure_date'])
+    days_of_stay = (departure_date - arrival_date).to_i
+    price = days_of_stay * hotel_prices[row['hotel']]
+    if result.key?(country_slug)
+      result[country_slug][:price] = result[country_slug][:price] + price
+    else
+      result[country_slug] = { country_name: country_name, price: price }
+    end
+  end
+
+  # Export to CSV
+  filename = "solution-5/question-2/countries-revenues.csv"
+  result = result.sort_by { |country, data| -data[:price] }.map { |country, data| [data[:country_name], data[:price]] }
+  result.unshift(['country', 'hotel_revenue'])
+  File.write(filename, result.map(&:to_csv).join)
+end
+
+countries_revenues(rows)
