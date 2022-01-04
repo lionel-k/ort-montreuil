56ae974bc929dd321bdc659772aad1b159ea87da
dani <soudry.daniel99@gmail.com>
2022-01-04 15:05:24 +0100

    question ticket per quarter

--- Daniel/Solution dataset-6/script-dataset-6-kpi-done-per-quarter.php
+++ Daniel/Solution dataset-6/script-dataset-6-kpi-done-per-quarter.php
@@ -10,54 +10,27 @@ $spreadsheet = $reader->load("dataset-6/kpis/dataset-6-tickets-kpis.csv");
 
 $tblKpis = $spreadsheet->getActiveSheet()->toArray();
 
-//Tableau avec la liste de dev
-$tblDev = [];
 //Tableau avec les kpis et leur trimestre respectif
 $tblTrimestre = [];
 for ($i = 1; $i <= 4; $i++) {
     $tblTrimestre[$i] = null;
 }
-//Tableau des résulats attendus : dev capacity
-$tblDevCapacity = [];
+//Tableau des résulats attendus : tickets done per quarter
+$tblDonePerQuarter = [];
 
 foreach ($tblKpis as $key => $kpi) {
     if ($key != 0) {
-        $trimestre = getTrimestre($kpi[0]);
+        $trimestre = getTrimestre($kpi[1]);
         $tblTrimestre[$trimestre][] = $kpi;
     }
 }
 
+
 foreach ($tblTrimestre as $key => $trimestre) {
-    if ($key == 1) {
-        $date_depart = '2021-01-01';
-        $date_fin = '2021-03-31';
-    } elseif ($key == 2) {
-        $date_depart = '2021-04-01';
-        $date_fin = '2021-06-30';
-    } elseif ($key == 3) {
-        $date_depart = '2021-07-01';
-        $date_fin = '2021-09-30';
-    } elseif ($key == 4) {
-        $date_depart = '2021-10-01';
-        $date_fin = '2021-12-31';
-    }
 
-    $nbrJourOuvres = get_nb_open_days(strtotime($date_depart), strtotime($date_fin));
+    $nbTicketDone = count($trimestre);
 
-
-    foreach ($trimestre as $keyTrimestre => $kpi) {
-
-        if ($key != 0) {
-            $nomDev = $kpi[3];
-            if (!in_array($nomDev, $tblDev)) {
-                $tblDev[] = $nomDev;
-            }
-        }
-    }
-
-    $tblDevCapacity["q" . $key] = $nbrJourOuvres * count($tblDev);
-
-    $tblDev = [];
+    $tblDonePerQuarter["q" . $key] = $nbTicketDone;
 }
 
 //Ecrire les données :
@@ -78,7 +51,7 @@ $celluleA = $lettreA . $chiffre;
 $spreadSheetWriter->getActiveSheet()
     ->setCellValue("A1", "quarter,dev_capacity");
 
-foreach ($tblDevCapacity as $key => $value) {
+foreach ($tblDonePerQuarter as $key => $value) {
     $spreadSheetWriter->getActiveSheet()->getRowDimension($chiffre)->setRowHeight(20);
 
     $spreadSheetWriter->getActiveSheet()
@@ -89,26 +62,11 @@ foreach ($tblDevCapacity as $key => $value) {
     $celluleA = $lettreA . $chiffre;
 }
 
-$nomFichier = "dev_capacity";
+$nomFichier = "tickets_done_per_quarter";
 
 $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadSheetWriter);
 $writer->save("question-kpis/" . $nomFichier . ".csv");
 
-
-
-// Renvoie le nbr de jours ouvrés entre deux dates
-function get_nb_open_days($date_start, $date_stop)
-{
-    $nb_days_open = 0;
-    while ($date_start <= $date_stop) {
-        // Si le jour suivant n'est ni un dimanche (0) ou un samedi (6), ni un jour férié, on incrémente les jours ouvrés	
-        if (!in_array(date('w', $date_start), array(0, 6))) {
-            $nb_days_open++;
-        }
-        $date_start = mktime(date('H', $date_start), date('i', $date_start), date('s', $date_start), date('m', $date_start), date('d', $date_start) + 1, date('Y', $date_start));
-    }
-    return $nb_days_open;
-}
 
 
 // Retourne à quel trimestre appartient une date
