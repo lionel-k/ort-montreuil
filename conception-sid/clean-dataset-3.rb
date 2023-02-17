52203367f2fe2a4aaa5e11f8bde774383a8192c4
Lionel Kubwimana <lionel.kubwimana@weareclimb.fr>
2023-02-17 09:41:17 +0100

    reformat

--- conception-sid/clean-dataset-3.rb
+++ conception-sid/clean-dataset-3.rb
@@ -1,10 +1,11 @@
 require 'csv'
+require 'pry'
 
 csv_options = { col_sep: ',', quote_char: '"', headers: :first_row }
-filepath    = 'dataset-3.csv'
+filepath = 'dataset-3.csv'
 
-VALUES = ["25CB99", "87A4E4", "CC2B77", "AED206", "9C88F3", "ECC5CE", "77AA3D", "D8470A", "647EED", "4E3B4C"]
-VALUES.join(",")
+VALUES = %w[25CB99 87A4E4 CC2B77 AED206 9C88F3 ECC5CE 77AA3D D8470A 647EED 4E3B4C]
+VALUES.join(',')
 
 def split_skus(row)
   skus, units = row['sku;units'].split(';')
@@ -13,34 +14,33 @@ def split_skus(row)
   combined = Hash[skus.zip(units)]
 end
 
-def print_split_skus
+def print_split_skus(row)
   combined = split_skus(row)
-  VALUES.map do |value|
-    combined[value]||0
-  end.join(",")
+  binding.pry
+  VALUES.map { |value| combined[value] || 0 }.join(',')
 end
 
 UNIT_PRICE = {
-  '25CB99' =>	31,
-  '87A4E4' =>	38,
-  'CC2B77' =>	46,
-  'AED206' =>	39,
-  '9C88F3' =>	25,
-  'ECC5CE' =>	36,
-  '77AA3D' =>	44,
-  'D8470A' =>	21,
-  '647EED' =>	40,
-  '4E3B4C' =>	28,
+  '25CB99' => 31,
+  '87A4E4' => 38,
+  'CC2B77' => 46,
+  'AED206' => 39,
+  '9C88F3' => 25,
+  'ECC5CE' => 36,
+  '77AA3D' => 44,
+  'D8470A' => 21,
+  '647EED' => 40,
+  '4E3B4C' => 28,
 }
 
-genres = ['M', 'F', 'Masc', 'Fem', 'Male', 'Female', 'masc', 'fem', '-']
+genres = %w[M F Masc Fem Male Female masc fem -]
 
 def is_female?(genre)
-  ['F', 'Fem', 'Female', 'fem'].include?(genre)
+  %w[F Fem Female fem].include?(genre)
 end
 
 def is_male?(genre)
-  ['M', 'Masc', 'male', 'masc', 'Male'].include?(genre)
+  %w[M Masc male masc Male].include?(genre)
 end
 
 def clean_genre(row)
@@ -59,7 +59,7 @@ def pm?(row)
   hour.to_i >= 12 && hour.to_i < 18
 end
 
-def between_17_and_21(row)
+def between_17_and_21?(row)
   hour, minute = row['hour'].split(':')
   hour.to_i >= 17 && hour.to_i < 21
 end
@@ -67,15 +67,8 @@ end
 def amount(row)
   skus_units = split_skus(row)
   sum = 0
-  skus_units.each do |sku, unit|
-    sum += UNIT_PRICE[sku] * unit.to_i
-  end
+  skus_units.each { |sku, unit| sum += UNIT_PRICE[sku] * unit.to_i }
   sum
 end
 
-
-CSV.foreach(filepath, csv_options) do |row|
-  p between_17_and_21?(row)
-end
-
-
+CSV.foreach(filepath, csv_options) { |row| p print_split_skus(row) }
